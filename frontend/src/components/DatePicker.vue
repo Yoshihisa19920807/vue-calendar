7b66ccdadadb4e7dc2b66ec9f4238344025dc81f
Yoshihisa Okada <yoshi19920807@gmail.com>
2021-08-31 15:28:36 +0900

    implement datepicker

---
+++ frontend/src/components/DatePicker.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <!-- templateにボタンとして表示したい文言
+  その後ろにボタンを押した際に表示される要素-->
+  <v-menu offset-y>
+    <!-- ここのonはevent listeners -->
+    <template v-slot:activator="{ on }">
+      <v-btn text v-on="on">{{ value || '日付を選択' }}</v-btn>
+    </template>
+
+    <!-- v-modelを使わないのはpropsのvalueが不変だから値を変えずにemitで親に返す -->
+    <!-- day-formatで「日」を消す -->
+    <!-- v-date-pickerはyyyy-mm-ddの形式しか認識しないのでreplaceする -->
+    <v-date-picker
+      :value="value.replace(/\//g, '-')"
+      @input="$emit('input', $event.replace(/-/g, '/'))"
+      no-title
+      locale="ja-ja"
+      :day-format="value => new Date(value).getDate()"
+    ></v-date-picker>
+    <!-- <p>{{ value }}</p> -->
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: 'DateForm',
+  props: ['value'],
+}
+</script>
